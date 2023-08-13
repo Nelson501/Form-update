9512d45c07a2b661ce62c9ba4b44cbd9073c9557
Nelson <111023854+Nelson501@users.noreply.github.com>
2023-08-14 00:21:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {useState, use} from 'react'
+
+import { Form } from './compoents/Form'
+
+function App() {
+
+return (
+    <>
+     <Form/>
+    </>
+  )
+}
+
+export default App
